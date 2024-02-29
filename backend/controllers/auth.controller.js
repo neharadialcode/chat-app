c24b067a5ddf15da8e7c7777234fbda776acc34c
Neha <neharadialcode@gmail.com>
2024-02-29 11:22:00 +0530

    signup controller added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,41 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
   try {
-    const { fullName, userName, password } = req.body;
-  } catch (error) {}
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        error: "Invalid password",
+      });
+    }
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({ error: "username is already exist" });
+    }
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      userName,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      userName: newUser.userName,
+      password: newUser.password,
+      gender: newUser.gender,
+    });
+  } catch (error) {
+    console.log(error.message, "Error in signup controller");
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
 };
 export const login = (req, res) => {
   console.log("login");
--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ import { connetDatabase } from "./db/connetDatabase.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.get("/", (req, res) => {
-  // root route http://localhost:4000
-  res.send("server is ready!");
-});
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
+
 app.listen(PORT, () => {
   connetDatabase();
   console.log("server running on port 4000");
 });
+
+// app.get("/", (req, res) => {
+//   root route http://localhost:4000
+//   res.send("server is ready!");
+// });
