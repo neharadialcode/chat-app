8fc38c5840b527a217992d61985e46122de9f312
Neha <neharadialcode@gmail.com>
2024-03-01 08:58:51 +0530

    error resolve

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { sendMessgae } from "../controllers/message.controller.js";
 import { protectRoute } from "../middlewares/protectRoute.js";
-const router = express();
+const router = express.Router();
 router.post("/send/:id", protectRoute, sendMessgae);
 
 export default router;
